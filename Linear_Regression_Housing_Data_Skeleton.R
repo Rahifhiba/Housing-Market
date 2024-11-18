9117d688f1c7a7db10fbffd52d6d37ec44713841
hepona <hepona10@gmail.com>
2024-11-18 23:56:15 +0100

    wip

---
+++ Linear_Regression_Housing_Data_Skeleton.R
@@ -0,0 +1,35 @@
+#Part 1 - Data analysis
+# Load required libraries
+# optional installation step, if you don't have the libraries installed:
+# install.packages(c("ggplot2", "dplyr","car"))
+library(ggplot2)
+library(dplyr)
+library(car)
+# 1. Load the data
+
+# 2. Summary statistics
+
+# 3. Calculate mean, mode and median, as well as the standard deviation for each variable
+# Mode function
+
+# Print results
+
+# 4. Compute the correlation between each pair of variables
+
+# Part 2 - Data visualization
+# Create a histogram of house prices with styling and 8 bins
+
+#Part 3 - Hypothesis testing
+# 1. Define "high" and "low" crime rates based on the median of the Crime.Rate variable
+
+# 2. Additional: Check for assumptions
+# For a t-test, it's essential to check for normality & homogeneity of variance.
+
+# Check normality for each group
+
+# 3. Conduct an independent two-sample t-test
+
+# 4. Print the results
+
+# Part 4 - Linear regression
+# for predicting the median home value with the average rooms variable
